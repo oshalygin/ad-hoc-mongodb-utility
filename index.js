4940e0713cd1c6720f19bb6326493b042e59b7f8
Oleg Shalygin <oshalygin@gmail.com>
2019-10-09 21:41:42 -0700

    Initial setup

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const mongo = require('mongodb');
 const Promise = require('bluebird');
-const template = require('./file.json');
+// const template = require('./file.json');
 
 const MongoClient = mongo.MongoClient;
 const ObjectId = mongo.ObjectId;
+const ObjectID = mongo.ObjectID;
 
 const LOCAL_URL = 'mongodb://localhost:27017/totem';
 const DEV_URL =
@@ -11,8 +12,18 @@ const DEV_URL =
 const PROD_URL =
   'mongodb+srv://totemadmin:olegoleg@prod-totem-mrp6w.gcp.mongodb.net/test?retryWrites=true&w=majority';
 const DB_NAME = 'totem';
+
+const ORGANIZATIONS = 'organizations';
 const QUESTIONNAIRE_TEMPLATES = 'questionnaireTemplates';
 const QUESTIONNAIRE_INSTANCES = 'questionnaireInstances';
+const SITES = 'sites';
+const CUSTOMERS = 'customers';
+const NETWORKS = 'networks';
+const USERS = 'users';
+const DEVICES = 'devices';
+const NMAP = 'nmap';
+const ANALYSIS = 'analysis';
+const MEDIA = 'media';
 
 function getAllDocs(collection) {
   return new Promise((resolve, reject) => {
@@ -34,46 +45,188 @@ MongoClient.connect(PROD_URL, async (err, client) => {
 
   try {
     const db = client.db(DB_NAME);
+
+    const analysisCollection = db.collection(ANALYSIS);
+    // const analysis = await getAllDocs(analysisCollection);
+
+    const customersCollection = db.collection(CUSTOMERS);
+    const customers = await getAllDocs(customersCollection);
+
+    const devicesCollection = db.collection(DEVICES);
+    // const devices = await getAllDocs(devicesCollection);
+
+    const mediaCollection = db.collection(MEDIA);
+    // const media = await getAllDocs(mediaCollection);
+
+    const networksCollection = db.collection(NETWORKS);
+    // const networks = await getAllDocs(networksCollection);
+
+    const nmapCollection = db.collection(NMAP);
+    // const nmap = await getAllDocs(nmapCollection);
+
+    const organizationsCollection = db.collection(ORGANIZATIONS);
+    // const organizations = await getAllDocs(organizationsCollection);
+
+    const instancesCollection = db.collection(QUESTIONNAIRE_INSTANCES);
+    // const instances = await getAllDocs(instancesCollection);
+
     const templatesCollection = db.collection(QUESTIONNAIRE_TEMPLATES);
+    // const templates = await getAllDocs(templatesCollection);
 
-    const templates = await getAllDocs(templatesCollection);
+    const sitesCollection = db.collection(SITES);
+    // const sites = await getAllDocs(sitesCollection);
 
-    // templates.forEach(template => {
-    //   if (template.name === 'BCET Enterprise') {
-    //     console.log(JSON.stringify(template));
-    //   }
-    // });
+    const usersCollection = db.collection(USERS);
+    // const users = await getAllDocs(usersCollection);
 
-    // Google APAC
-    // org: ObjectId("5d29d6863c7ba7000149eea5")
-    // user: ObjectId("5d29df557140f40001eb185e")
-
-    // Google EMEA
-    // org: ObjectId("5d29debd3c7ba7000149eea6")
-    // user: ObjectId("5d2a023f7140f40001eb185f")
-
-    // Google Americas
-    // org: ObjectId("5d29ded17140f40001eb185d")
-    // user: ObjectId("5d2a04207140f40001eb1860")
-
-     // Totem Buildings
-    // org: ObjectId("5cb770e6a275c500012ab2e9")
-    // user: ObjectId("5d2667343c7ba7000149eea0")
-
-
-
-
-    templatesCollection.insertOne({
-      ...template,
-      organizationId: ObjectId("5d289c42461ab90001ba356c"),
-      updatedBy: '5d3a819d877869cf0c2d0d94',
-      createdBy: '5d3a819d877869cf0c2d0d94',
-      createdAt: new Date(),
-      updatedAt: new Date(),
+    let oldCustomerOrgId;
+    const newOrganizationId = ObjectId('5b2f1be53911bd000175043a');
+    console.log(newOrganizationId.toHexString());
+    customers.forEach(customer => {
+      if (customer.name === 'Kaiser Permanente') {
+        console.log(JSON.stringify(customer, null, 2));
+        oldCustomerOrgId = ObjectId(customer.organizationId);
+      }
     });
+
+    // update analysis collection organizationId with ObjectId
+
+    const analysisResult = await analysisCollection.updateMany(
+      { organizationId: oldCustomerOrgId },
+      {
+        $set: {
+          organizationId: newOrganizationId,
+        },
+      },
+    );
+
+    console.log(analysisResult);
+
+    // update customer collection organizationId with objectId
+
+    const customerResult = await customersCollection.updateMany(
+      { organizationId: oldCustomerOrgId },
+      {
+        $set: {
+          organizationId: newOrganizationId,
+        },
+      },
+    );
+
+    console.log(customerResult);
+
+    // update devices collection organizationId with objectId
+
+    const devicesResult = await devicesCollection.updateMany(
+      { organizationId: oldCustomerOrgId },
+      {
+        $set: {
+          organizationId: newOrganizationId,
+        },
+      },
+    );
+
+    console.log(devicesResult);
+
+    // update media collection organizationId with objectId
+
+    const mediaResult = await mediaCollection.updateMany(
+      { organizationId: oldCustomerOrgId },
+      {
+        $set: {
+          organizationId: newOrganizationId,
+        },
+      },
+    );
+
+    console.log(mediaResult);
+
+    // update networks collection organizationId with objectId
+
+    const networksResult = await networksCollection.updateMany(
+      { organizationId: oldCustomerOrgId },
+      {
+        $set: {
+          organizationId: newOrganizationId,
+        },
+      },
+    );
+
+    console.log(networksResult);
+
+    // update nmap collection organizationId with objectId
+
+    const nmapResult = await nmapCollection.updateMany(
+      { organizationId: oldCustomerOrgId },
+      {
+        $set: {
+          organizationId: newOrganizationId,
+        },
+      },
+    );
+
+    console.log(nmapResult);
+
+    // update questinnaireInstances collection organizationId with objectId
+
+    const instancesResult = await instancesCollection.updateMany(
+      { organizationId: oldCustomerOrgId },
+      {
+        $set: {
+          organizationId: newOrganizationId,
+        },
+      },
+    );
+
+    console.log(instancesResult);
+
+    // update templates collection organizationId with objectId
+
+    const templatesResult = await templatesCollection.updateMany(
+      { organizationId: oldCustomerOrgId },
+      {
+        $set: {
+          organizationId: newOrganizationId,
+        },
+      },
+    );
+
+    console.log(templatesResult);
+
+    // update sites collection organizationId with objectId
+
+    const sitesResult = await sitesCollection.updateMany(
+      { organizationId: oldCustomerOrgId },
+      {
+        $set: {
+          organizationId: newOrganizationId,
+        },
+      },
+    );
+
+    console.log(sitesResult);
+
+    // const sitesResult = await sitesCollection.updateMany(
+    //   { organizationId: oldCustomerOrgId },
+    //   {
+    //     $set: {
+    //       organizationId: newOrganizationId,
+    //     },
+    //   },
+    // );
+
+    // console.log(sitesResult);
+
+    // templatesCollection.insertOne({
+    //   ...template,
+    //   organizationId: ObjectId('5d4591100f9272000114fe03'),
+    //   updatedBy: '5d3a819d877869cf0c2d0d94',
+    //   createdBy: '5d3a819d877869cf0c2d0d94',
+    //   createdAt: new Date(),
+    //   updatedAt: new Date(),
+    // });
     client.close();
   } catch (e) {
-    console.error(e);
     console.error(e);
     client.close();
   }
